290a5b39e32ca5cf7baa7814fed613b94561da1a
Charles <charlescon@live.com>
2023-11-18 17:28:09 -0600

    top and bottom nav added to home

--- src/assets/components/Home/Home.js
+++ src/assets/components/Home/Home.js
@@ -3,14 +3,20 @@ import styles from './Home.module.scss'
 import house from '../../images/home.png'
 import bell from '../../images/bell.png'
 import user from '../../images/user.png'
+import logo from '../../images/eye.webp'
+import gear from '../../images/gear.png'
 
 function Home() {
   return (
     <>
+        <nav className={styles.topNav}> 
+            <img src={logo} alt='logo'/>
+            <a href='/home'><img src={gear} alt='settings'/></a>
+        </nav>
         <div className={styles.homeContainer}>
 
         </div>
-        <nav role='navigation'>
+        <nav className={styles.bottomNav} role='navigation'>
             <a href='/home'><img src={house} alt='home'/></a>
             <a href='/home'><img src={bell} alt='notifications'/></a>
             <a href='/home'><img src={user} alt='user'/></a>
