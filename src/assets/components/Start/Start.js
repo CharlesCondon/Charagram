827b035b315351e43b8f03a6913be95b48c623f8
Charles <charlescon@live.com>
2023-11-19 13:29:08 -0600

    remember non-guest user to redirect to homepage

--- src/assets/components/Start/Start.js
+++ src/assets/components/Start/Start.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import styles from './Start.module.scss'
 import Footer from '../Footer/Footer'
+import { Navigate } from 'react-router-dom'
 
 function Start() {
     const [token, setToken] = useState(false);
@@ -8,13 +9,21 @@ function Start() {
     useEffect(() => {
         let token = window.localStorage.getItem("token");
         console.log(token);
-        if(token) {
+        if(token !== "guest" && token !== null) {
+            console.log('setting new token');
             setToken(true);
         }
     }, []);
 
+    const setGuest = () => {
+        const guestToken = 'guest';
+        window.localStorage.setItem("token", guestToken);
+    }
+
     return (
         <>
+            {token ? <Navigate to="/home" replace={true} /> 
+            :<> 
             <div className={styles.loginContainer}>
                 <div className={styles.imageContainer}>
                     <img className={styles.headerImg} src='./eye.webp' alt='logo'/>
@@ -38,10 +47,13 @@ function Start() {
                         </div>
                     </div>
             
-                    <a className={styles.signinBtn} href='/home?user=guest'>Continue as Guest</a>
+                    <a className={styles.signinBtn} onClick={setGuest} href='/home?user=guest'>Continue as Guest</a>
                 </div>
             </div>
             <Footer/>
+            </>
+            }
+            
         </>
     )
 }
