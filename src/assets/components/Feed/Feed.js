0aacad7433297235ed19cc19ac0355e3aa27e41a
Charles <charlescon@live.com>
2023-11-20 19:05:38 -0600

    anonymous user allowed and env variables made

--- src/assets/components/Feed/Feed.js
+++ src/assets/components/Feed/Feed.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth, signInAnonymously } from "firebase/auth";
 import styles from './Feed.module.scss'
 import Post from '../Post/Post'
 import PostBox from '../PostBox/PostBox'
@@ -7,17 +8,28 @@ import db from '../../../db/firebase'
 
 
 function Feed() {
-
     const [posts, setPosts] = useState([]);
+    const auth = getAuth();
+    
 
     useEffect(() => {
-        async function getPosts(db) {
-            const postCol = collection(db, 'posts');
-            const postSnap = await getDocs(postCol);
-            const postList = postSnap.docs.map(doc => doc.data());
-            setPosts(postList);
-        }
-        getPosts(db);
+        signInAnonymously(auth)
+            .then(() => {
+                async function getPosts(db) {
+                    const postCol = collection(db, 'posts');
+                    const postSnap = await getDocs(postCol);
+                    const postList = postSnap.docs.map(doc => doc.data());
+                    setPosts(postList);
+                }
+                getPosts(db);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode);
+                console.log(errorMessage);
+        });
+        
     }, []);
 
     return (
