07644b31016fdbd1547e5346d634d9fe888e9b83
Charles <charlescon@live.com>
2023-11-21 01:47:02 -0600

    added basic user signup functionality

--- src/assets/components/Signup/Signup.js
+++ src/assets/components/Signup/Signup.js
@@ -1,24 +1,64 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styles from './Signup.module.scss'
 import Footer from '../Footer/Footer'
+import db from '../../../db/firebase'
+import { collection, addDoc, doc, getDoc } from "firebase/firestore/lite"; 
 
 function Signup() {
+	const [email, setEmail] = useState("");
+	const [name, setName] = useState("");
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		
+
+		async function checkUser() {
+			const docRef = doc(db, "users", username);
+			const docSnap = await getDoc(docRef);
+			if (docSnap.exists()) {
+				console.log('user exists');
+				setUsername("")
+			}
+			else {
+				try {
+					async function docRef(db)  {
+						await addDoc(collection(db,"users"), {
+							avatar: "",
+							displayName: name,
+							username: username,
+							email: email,
+							verified: false,
+							password: password
+						})
+					}
+					docRef(db);
+				} catch (e) {
+					console.error("Error adding document: ", e);
+				}
+			}
+		}
+		checkUser();
+	}
+
 	return (
 		<>
 			<div className={styles.signupContainer}>
 					<img src='./eye.webp' alt='logo'/>
 					<h2>Sign up to start sharing positivity with your friends.</h2>
-					<form action=''>
+					<form>
 							<label for='email'></label>
-							<input className={styles.input} type='text' name='email' placeholder='Email'></input>
+							<input onChange={(e) => setEmail(e.target.value)} value={email} className={styles.input} type='email' name='email' placeholder='Email'></input>
 							<label for='name'></label>
-							<input className={styles.input} type='text' name='name' placeholder='Full Name'></input>
+							<input onChange={(e) => setName(e.target.value)} value={name} className={styles.input} type='text' name='name' placeholder='Display Name'></input>
 							<label for='username'></label>
-							<input className={styles.input} type='text' name='username' placeholder='Username'></input>
+							<input onChange={(e) => setUsername(e.target.value)} value={username} className={styles.input} type='text' name='username' placeholder='Username'></input>
 							<label for='password'></label>
-							<input className={styles.input} type='password' name='password' placeholder='Password'></input>
+							<input onChange={(e) => setPassword(e.target.value)} value={password} className={styles.input} type='password' name='password' placeholder='Password'></input>
 							<div className={styles.submitBtn}>
-								<input type="submit" value="Sign Up"/>
+								<input onClick={handleSubmit} type="submit" value="Sign Up"/>
 							</div>
 					</form>
 					<div>
