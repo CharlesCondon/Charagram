121b9bb7d0bcacc6d8853496169162839f13f4d9
Charles <charlescon@live.com>
2023-11-20 19:09:06 -0600

    added db file

---
+++ src/db/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore} from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+    apiKey: `${process.env.REACT_APP_DB_KEY}`,
+    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+    storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_MESSAGE_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_APP_ID}`,
+    measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export default db;
