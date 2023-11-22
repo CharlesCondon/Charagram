94c757fccb2a0acf143ae2e7215d7a1d131662f3
Charles <charlescon@live.com>
2023-11-22 12:26:47 -0600

    fixed lg navbar words and added analytics tag

--- src/assets/components/NavbarOption/NavbarOption.js
+++ src/assets/components/NavbarOption/NavbarOption.js
@@ -2,21 +2,21 @@ import { Link } from 'react-router-dom'
 import styles from './NavbarOption.module.scss'
 
 function NavbarOption({active, text, src, user}) {
-    
+    let url = text;
     if (text === "home") {
-        text = "../home";
+        url = "../home";
     }
     else if (text !== "post" && text !== "notifications") {
-        text = "../user/" + text;
+        url = "../user/" + url;
     }
     else {
-        text = "../home/" + text;
+        url = "../home/" + url;
     }
     //console.log(user)
 
     return (
         <div className={`${styles.navOptionContainer} ${active && styles['navOption--active']}`}>
-            <Link to={text} >
+            <Link to={url} >
                 <img src={src} alt={text}  />
                 <h2>{text}</h2>
             </Link>
