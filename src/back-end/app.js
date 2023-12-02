93fd1366a1421562acda0e68c8ef1353866c0754
Charles <charlescon@live.com>
2023-12-01 19:44:48 -0600

    axios change

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.post('https://charlesgram.vercel.app', (req,res) => {
+app.post('https://charlesgram.vercel.app', cors(), (req,res) => {
     const analyzeParams = {
         'text': `${req.body.text}`,
         'features': {
